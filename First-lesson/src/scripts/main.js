9b9be58f7fa6b790d065ca8133d1c2f05e1fac1e
Fafot <zarytskyi.s@hotmail.com>
2022-03-25 13:46:21 +0200

    my first commit

---
+++ First-lesson/src/scripts/main.js
@@ -0,0 +1,27 @@
+function введитеВозраст() {
+    // Вызвать promt, в который нужно указать возраст
+    //const age = prompt("Введите ваш возраст");
+    const age = "36";
+    const parsedAge = Number.parseInt(age, 10);
+    const currentTime = new Date();
+    textAge = parsedAge
+    if (Number.isNaN(parsedAge)) {
+        console.log("Age is invalid");
+    } else {
+        console.log(`Ваш возраст ${parsedAge}`);
+    }
+    if (parsedAge >= 35) {
+        console.log("Nezalejnosti sqr");
+    }
+    if (currentTime.getSeconds() % 2 === 0 && parsedAge%2!=0) {
+        console.log(currentTime.getMilliseconds()%parsedAge)
+    
+    }
+if(currentTime.getSeconds()%2!=0 && parsedAge%2===0) {console.log(`${parsedAge}`)}
+
+    // Если возраст больше 35, то вызвать alert с адресом ближайщего для тебя палеонтологического музея и закончить выполнение функции
+    // Если текущая секунда четная и возраст нечётный, то вызвать alert с содержанием остаток деления времени в миллисекундах на возраст и закончить выполнение функции
+    // Если секунда нечётная, а возраст чётный, то вызвать alert с содержанием возраста в строчном виде. Например, 30 - тридцить, 24 - двадцать четыре, 7 - семь
+    // Если ничего из этого не подошло, то написать в консоль (console.log) возраст и время
+}
+введитеВозраст();
